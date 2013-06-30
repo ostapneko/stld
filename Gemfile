d8aad89df9593a505cbcf7cd9e8734615e599cc6
ostapneko <ostapneko@gmail.com>
2013-07-01 00:50:26 +0100

    Add unique task deletion

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source 'https://rubygems.org'
 
 gem 'sinatra'
 gem 'sinatra-contrib'
+gem 'rack-flash3'
 gem 'sequel'
 gem 'pg'
 
--- stld.rb
+++ stld.rb
@@ -1,9 +1,22 @@
 require 'sinatra'
 require 'sinatra/content_for'
+require 'rack-flash'
 
 require_relative 'models/recurring_task'
 require_relative 'models/unique_task'
 require_relative 'services/task_service'
+
+enable :sessions
+use Rack::Flash
+
+helpers do
+  def delete_task(task_class)
+    service = TaskService.new
+    id = params[:id].to_i
+    flash[:errors], flash[:notice] = service.try_delete_task(params[:id], RecurringTask)
+    redirect to('/tasks')
+  end
+end
 
 get '/' do
   erb :"home/index"
@@ -15,6 +28,14 @@ end
 
 post '/recurring_task' do
   service = TaskService.new
-  @errors, @msg = service.try_create_recurring_task(params)
-  erb :"tasks/index"
+  flash[:errors], flash[:notice] = service.try_create_recurring_task(params)
+    redirect to('/tasks')
+end
+
+delete '/recurring_task/:id' do
+  delete_task(RecurringTask)
+end
+
+delete '/unique_task/:id' do
+  delete_task(UniqueTask)
 end
