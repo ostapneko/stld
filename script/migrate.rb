65de3b38f9052bb26847b3c9b995a370a108d403
ostapneko <ostapneko@gmail.com>
2013-08-08 20:31:46 +0100

    Change migration code

---
+++ script/migrate.rb
@@ -0,0 +1,22 @@
+#!/bin/env ruby
+
+require 'yaml'
+
+class Migrator
+  def initialize(args)
+    @destroy = args[0] == "destroy"
+  end
+
+  def run
+    env = ENV['RACK_ENV'] || "development"
+    config = YAML.load(File.read("config/database.yml"))[env]
+    connection_string = "postgres://#{config['username']}:#{config['password']}@#{config['host']}:#{config['port']}/#{config['database']}"
+    options = "-m migrations"
+    options << " -M 0" if @destroy
+    `sequel #{options} #{connection_string}`
+  end
+end
+
+if $0 == __FILE__
+  Migrator.new(ARGV).run
+end
--- script/recreate_db.rb
+++
@@ -1,6 +0,0 @@
-require 'yaml'
-env = ENV['RACK_ENV'] || "development"
-config = YAML.load(File.read("config/database.yml"))[env]
-connection_string = "postgres://#{config['username']}:#{config['password']}@#{config['host']}:#{config['port']}/#{config['database']}"
-`sequel -M 0 -m migrations #{connection_string}`
-`sequel -m migrations #{connection_string}`
