40b6d3cd57fb63f6a95af4f9f1e71832a0516a4c
Thomas Franquelin <ostapneko@gmail.com>
2015-08-05 10:36:08 +0100

    Use DATABASE_URL directly

--- config/connection.rb
+++ config/connection.rb
@@ -5,6 +5,6 @@ $stderr.puts "WARNING: The RACK_ENV environment variable is not set. Assuming th
 
 env = ENV['RACK_ENV'] || "development"
 
-connection_string = YAML.load(File.read("config/database.yml"))[env]
+connection_string = ENV['DATABASE_URL'] || YAML.load(File.read("config/database.yml"))[env]
 
 DB = Sequel.connect(connection_string)
